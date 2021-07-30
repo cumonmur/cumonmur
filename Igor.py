ccc6fd8874c38b41e6c5658741c30f41ddfdd14d
igorek-murrom <murzingh@yandex.ru>
2021-07-30 23:49:22 +1000

    add regulator for gate

--- Igor.py
+++ Igor.py
@@ -1,6 +1,5 @@
 import pymurapi as mur
 import cv2 as cv
-import math
 import time
 import numpy as np
 
@@ -14,6 +13,32 @@ max_hsv_yellow = (70, 255, 255)
 def show():
     ok, frame = cap.read()
     return frame
+
+def clamp(val):
+    if val > 100:
+        return 100
+    if val < -100:
+        return -100
+    return int(val)
+
+
+def clamp_angle(angle):
+    if angle > 180:
+        return angle - 360
+    if angle < - 180:
+        return angle + 360
+    return int(angle)
+
+
+def keep_yaw(yaw_to_set, speed, k = 1):
+    current_yaw = auv.get_yaw()
+    e = clamp_angle(current_yaw - yaw_to_set)
+    print(e)
+    res = e * k
+    auv.set_motor_power(1, clamp(res) + speed)
+    auv.set_motor_power(0, clamp(-res) - speed)
+    time.sleep(0.01)
+    return e
 
 def gate():
     list = find_shape(show(), low_hsv_yellow, max_hsv_yellow)
@@ -31,7 +56,7 @@ def find_shape(img, hsv_min, hsv_max, area1=400):  # Поиск Фигур
     img_bin = cv.inRange(img_hsv, hsv_min, hsv_max)
     mur_view.show(img_bin, 1)
     cnt, _ = cv.findContours(img_bin, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-    list_cont = []
+    list_cont = [0,None]
     if cnt:
         for c in cnt:
             area = cv.contourArea(c)
@@ -51,5 +76,28 @@ def find_shape(img, hsv_min, hsv_max, area1=400):  # Поиск Фигур
     mur_view.show(img, 0)
     return list_cont
 
+def go_yaw(yaw, speed, sec):
+    time_flag = time.time()
+    while time.time() - time_flag < sec:
+        keep_yaw(yaw,speed)
+        cv.waitKey(1)
+
+def go_center_to_x(x, xcenter = 320,sec = 1, k = 1):
+    time_flag = time.time()
+    while time.time() - time_flag < sec:
+        e = xcenter - x
+        print(e)
+        res = e * k
+        auv.set_motor_power(1, clamp(res))
+        auv.set_motor_power(0, clamp(-res))
+        time.sleep(0.01)
+    return auv.get_yaw()
+
 while True:
-    gate()
+    list = gate()
+    cnt, x = list
+    if x is not None:
+        yaw = go_center_to_x(x)
+        go_yaw(yaw, 50, 5)
+
+
